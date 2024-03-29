89cef722b3b7d34bd0394dafe1adfa7fd6c44560
Devjean2000 <112570027+Devjean2000@users.noreply.github.com>
2024-03-29 17:40:55 -0500

    Add files via upload

---
+++ LISTADOCLIENTES.cbl
@@ -0,0 +1,24 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. LISTADOCLIENTES.
+       AUTHOR. JEAN BUELVAS.
+       INSTALLATION. Munive Inc.
+       DATE-WRITTEN. 29 de octubre del 2024.
+       SECURITY. Este programa es de licencia libre.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER.DESKTOP-TSU11JH.
+       OBJECT-COMPUTER.DESKTOP-TSU11JH.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Este programa tiene como fin listar clientes"
+            STOP RUN.
+       END PROGRAM  LISTADOCLIENTES.
