62f58d53f7030f56f0706138052956cebb814d35
Devjean2000 <112570027+Devjean2000@users.noreply.github.com>
2024-03-29 17:43:51 -0500

    Add files via upload
    Mi primer programa hecho en COBOL. El viejo y conocido: ¡Hola, Mundo!

---
+++ Holamundo.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Jean Paul
+      * Date: 14/03/2024
+      * Purpose: Practice
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HOLA-MUNDO.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "¡Hola, Mundo!"
+            STOP RUN.
+       END PROGRAM HOLA-MUNDO.
